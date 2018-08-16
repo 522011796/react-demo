ec4e0fe1c2fcbf43193c3b600125b1606cca2d0e
9bf_100006791283_sanguing <9bf_100006791283_sanguing@git.cloud.tencent.com>
2018-08-16 08:49:30 +0800

    修改样式

--- src/components/login.js
+++ src/components/login.js
@@ -1,34 +1,56 @@
 import React, { Component } from 'react';
+import './login.css'
+import { Tabs,Input,Button } from 'antd';
+const TabPane = Tabs.TabPane;
 
 //=====组件=====
 
 class Login extends Component {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            mainStyle:{}
+        };
+    }
     render() {
         return (
-            <div>
-                <h3>登录页面</h3>
-                <div>
-                    用户名<input type="text" />
-                </div>
-                <div>
-                    密码<input type="text" />
-                </div>
-                <div>
-                    <button onClick={this.goLogin}>登录</button>
+            <div className='loginMain' style={this.state.mainStyle}>
+                <div className='loginContent'>
+                    <div className='loginTitle'>ricky</div>
+                    <div className='loginOpr'>
+                        <Tabs defaultActiveKey="1">
+                            <TabPane tab="账号" key="1">
+                                <div>
+                                    <Input size='large' placeholder="" />
+                                </div>
+                                <div style={{marginTop:'10px'}}>
+                                    <Input size='large' placeholder="" />
+                                </div>
+                                <div style={{marginTop:'10px'}}>
+                                    <Button size='large' block style={{background:'#41464b',color:'#ffffff',fontSize:'12px'}} onClick={this.goLogin}>登录</Button>
+                                </div>
+                            </TabPane>
+                            <TabPane tab="微信" key="2" disabled></TabPane>
+                            <TabPane tab="钉钉" key="3" disabled>钉钉</TabPane>
+                        </Tabs>
+                    </div>
                 </div>
             </div>
         );
     }
 
     goLogin(){
-        alert("开始登录");
-        //this.props.history.push("./App/App.js");
         window.location.href="/"
     }
 
     componentDidMount() {
-
+        var h = document.documentElement.clientHeight || document.body.clientHeight;
+        this.setState({
+            mainStyle : {
+                height: h,
+                overflowY: 'auto'
+            }
+        });
     }
 
 }
